1ab01004aa96f9f1f656367a9866e63077eb7b86
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2024-03-03 17:08:36 -0500

    add ex2

---
+++ ex2/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const (
+	defaultPort = ":3000"
+)
+
+func main() {
+	pathToUrl := map[string]string{
+		"/google":   "https://www.google.com",
+		"/facebook": "https://www.facebook.com",
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", HomeHandler(pathToUrl))
+	err := http.ListenAndServe(defaultPort, MapHandler(pathToUrl, mux))
+	if err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+}
+
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if url, ok := pathsToUrls[r.URL.Path]; ok {
+			http.Redirect(w, r, url, http.StatusSeeOther)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}
+}
+
+func HomeHandler(m map[string]string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Content-Type", "text/html")
+		homePage := "<h1>Redirects</h1><ul>"
+		for path, url := range m {
+			homePage += fmt.Sprintf("<li>%s : %s</li>", path, url)
+		}
+		homePage += "</ul>"
+
+		w.Write([]byte(homePage))
+	}
+}
